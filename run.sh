7542c21668587bc24d8fc8cfe33f8c7a9c211375
Takuya Mukohira <takuya.mk96@gmail.com>
2018-05-16 11:06:44 +0900

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home mk-jenkins
+#!/usr/bin/env sh
+docker run -p 8080:8080 -p 50000:50000 -v `pwd`/jenkins_home:/var/jenkins_home mk-jenkins
