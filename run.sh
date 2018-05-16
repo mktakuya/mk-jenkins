40e0bcb44cd3379dc722f29fb303032f536d59a8
Takuya Mukohira <takuya.mk96@gmail.com>
2018-05-16 12:02:59 +0900

    Add --restart=always option

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run -p 8080:8080 -p 50000:50000 -v `pwd`/jenkins_home:/var/jenkins_home mk-jenkins
+docker run --restart=always -d -p 8080:8080 -p 50000:50000 -v `pwd`/jenkins_home:/var/jenkins_home mk-jenkins
